accf48c77bf1122a2cdbb88a7ce952ebe3854ccb
John K <ibortsov14@gmail.com>
2019-11-30 00:03:13 +0300

    Updates

---
+++ precache-manifest.e41c48cff7201e4fd6dbad01c29fd973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016560eb578768203828711b952c0ba6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "faaa237b85851ee5a44c",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "b88c9b57d435230e51b0",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "faaa237b85851ee5a44c",
+    "url": "./static/js/2.896007a7.chunk.js"
+  },
+  {
+    "revision": "b88c9b57d435230e51b0",
+    "url": "./static/js/main.5c9d7be0.chunk.js"
+  },
+  {
+    "revision": "5178effd5d2a449fe93d",
+    "url": "./static/js/runtime-main.748487dd.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
